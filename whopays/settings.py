5e3bb5c9fe6d2753310e2ef2a8a0a076814cd2f0
David Miller <david@deadpansincerity.com>
2014-01-16 07:17:40 +0000

    Stringify static assets dir

--- whopays/settings.py
+++ whopays/settings.py
@@ -58,7 +58,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    ROOT/'static',
+    str(ROOT/'static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
