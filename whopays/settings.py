0cf066e1d4cbc592c76f639140f30f3be3290336
David Miller <david@deadpansincerity.com>
2015-11-16 11:05:17 +0000

    Add www prefix

--- whopays/settings.py
+++ whopays/settings.py
@@ -164,7 +164,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 CSRF_COOKIE_NAME = 'XSRF-TOKEN'
 DEFAULT_FROM_EMAIL = 'hello@whopaysthisdoctor.org'
 CONTACT_EMAIL = DEFAULT_FROM_EMAIL
-DEFAULT_DOMAIN = 'whopaysthisdoctor.org'
+DEFAULT_DOMAIN = 'www.whopaysthisdoctor.org'
 
 EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
 
