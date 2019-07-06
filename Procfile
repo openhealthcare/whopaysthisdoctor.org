8164362e92232e46d441af18f71949495878d3b3
fredkingham <fredkingham@gmail.com>
2019-07-06 12:42:52 +0100

    update requirements.txt

---
+++ Procfile
@@ -0,0 +1,3 @@
+release: python manage.py migrate --noinput
+web: gunicorn whopaysthisdoctor.wsgi
+
--- whopaysthisdoctor/settings.py
+++ whopaysthisdoctor/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,11 +26,14 @@ SECRET_KEY = '8ihqpiox^&7u%9t8b%8(&4#fr78g%c4x!e&tds^r5po)^64ndl'
 STATIC_ROOT = "static"
 STATIC_URL = "/static/"
 WHITENOISE_STATIC_PREFIX = "/static/"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'whopaysthisdoctor2.herokuapp.com',
+]
 
 
 # Application definition
@@ -48,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -80,12 +85,7 @@ WSGI_APPLICATION = 'whopaysthisdoctor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {'default': dj_database_url.config(default='sqlite:///whopays.sqlite')}
 
 
 # Password validation
